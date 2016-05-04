0854717dc3c7093b8448bec2631dd1ef5a2fecf6
MaxBenoit <benoit.maxime@hotmail.com>
2016-05-04 18:19:16 -0400

    First Commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.mousePosition.x > Screen.width / 2 + Screen.width / 8)
+        {
+            GetComponent<Transform>().Rotate(Vector3.up, 0.5f);
+        }
+        else if (Input.mousePosition.x < Screen.width / 2 - Screen.width / 8)
+        {
+            GetComponent<Transform>().Rotate(Vector3.up, -0.5f);
+        }
+
+        if (Input.mousePosition.y > Screen.height / 2 + Screen.height / 8)
+        {
+            GetComponent<Transform>().Rotate(Vector3.right, -0.5f);
+        }
+        else if (Input.mousePosition.y < Screen.height / 2 - Screen.height / 8)
+        {
+            GetComponent<Transform>().Rotate(Vector3.right, 0.5f);
+        }
+	
+	}
+}
---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ManageMesh();
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Transform>().Rotate(Vector3.up, -1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Transform>().Rotate(Vector3.up, 1f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-5 * Mathf.Sin(GetComponent<Transform>().rotation.y), 0, -5 * Mathf.Cos(GetComponent<Transform>().rotation.y));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(5 * Mathf.Sin(GetComponent<Transform>().rotation.y), 0, 5 * Mathf.Cos(GetComponent<Transform>().rotation.y));
+        }
+	}
+
+    void ManageMesh()
+    {
+        if (Input.mousePosition.x > Screen.width / 2 + Screen.width / 8)
+        {
+            GetComponentInChildren<Transform>().Rotate(Vector3.up, 1f);
+        }
+        else if (Input.mousePosition.x < Screen.width / 2 - Screen.width / 8)
+        {
+            GetComponentInChildren<Transform>().Rotate(Vector3.up, -1f);
+        }
+
+        if (Input.mousePosition.y > Screen.height / 2 + Screen.height / 8)
+        {
+            GetComponentInChildren<Transform>().Rotate(Vector3.right, 1f);
+        }
+        else if (Input.mousePosition.y < Screen.height / 2 - Screen.height / 8)
+        {
+            GetComponentInChildren<Transform>().Rotate(Vector3.right, -1f);
+        }
+    }
+}
