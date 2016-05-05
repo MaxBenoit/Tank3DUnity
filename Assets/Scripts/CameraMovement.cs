65a7c02531348e6663204a99fd96c16c2096339b
MaxBenoit <benoit.maxime@hotmail.com>
2016-05-05 17:59:56 -0400

    Deleting Bullets

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour 
 {
     Transform transform;
+    public GameObject target;
 
 	// Use this for initialization
 	void Start () 
@@ -16,20 +17,20 @@ public class CameraMovement : MonoBehaviour
     {
         if (Input.mousePosition.x > Screen.width / 2 + Screen.width / 8)
         {
-            GetComponent<Transform>().Rotate(Vector3.up, 0.5f);
+            GetComponent<Transform>().Rotate(target.transform.up, 0.5f);
         }
         else if (Input.mousePosition.x < Screen.width / 2 - Screen.width / 8)
         {
-            GetComponent<Transform>().Rotate(Vector3.up, -0.5f);
+            GetComponent<Transform>().Rotate(target.transform.up, -0.5f);
         }
 
         if (Input.mousePosition.y > Screen.height / 2 + Screen.height / 8)
         {
-            GetComponent<Transform>().Rotate(Vector3.right, -0.5f);
+            GetComponent<Transform>().Rotate(transform.right, 0.5f);
         }
         else if (Input.mousePosition.y < Screen.height / 2 - Screen.height / 8)
         {
-            GetComponent<Transform>().Rotate(Vector3.right, 0.5f);
+            GetComponent<Transform>().Rotate(transform.right, -0.5f);
         }
 	
 	}
