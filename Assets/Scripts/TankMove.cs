78c026282960de1dcc7432bff9d46a6ba9bac49d
HazWard <starlysolon@gmail.com>
2016-05-05 17:40:38 -0400

    TankMove script added

---
+++ Assets/Scripts/TankMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMove : MonoBehaviour
+{
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    void Update()
+    {
+        float translation = -Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        GetComponent<Transform>().Translate(0, 0, translation);
+        GetComponent<Transform>().Rotate(0, rotation, 0);
+    }
+}
