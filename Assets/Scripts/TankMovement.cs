a99ef6ecf6b2f147e8c28249cc091d56274154a5
MaxBenoit <benoit.maxime@hotmail.com>
2016-05-04 21:05:56 -0400

    Terrain modifications

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -1,60 +1,124 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class TankMovement : MonoBehaviour 
+public class TankMovement : MonoBehaviour
 {
-    float RotationY;
+    public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
+    public float m_Speed = 12f;                 // How fast the tank moves forward and back.
+    public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+    public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
+    public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
+    public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
+    public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //ManageMesh();
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            GetComponent<Transform>().Rotate(Vector3.up, -1f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            GetComponent<Transform>().Rotate(Vector3.up, 1f);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(-100 * Mathf.Deg2Rad * Mathf.Sin(GetComponent<Transform>().rotation.y), 0, -100 * Mathf.Deg2Rad * Mathf.Cos(GetComponent<Transform>().rotation.y));
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(100 * Mathf.Sin(GetComponent<Transform>().rotation.y), 0, 100 * Mathf.Cos(GetComponent<Transform>().rotation.y));
-        }
-	}
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;             // The current value of the turn input.
+    private float m_OriginalPitch = 0f;              // The pitch of the audio source at the start of the scene.
 
-    void ManageMesh()
+
+    private void Awake ()
     {
-        if (Input.mousePosition.x > Screen.width / 2 + Screen.width / 8)
-        {
-            GetComponentInChildren<Transform>().Rotate(Vector3.up, 1f);
-        }
-        else if (Input.mousePosition.x < Screen.width / 2 - Screen.width / 8)
-        {
-            GetComponentInChildren<Transform>().Rotate(Vector3.up, -1f);
-        }
+        m_Rigidbody = GetComponent<Rigidbody> ();
+    }
 
-        if (Input.mousePosition.y > Screen.height / 2 + Screen.height / 8)
+
+    private void OnEnable ()
+    {
+        // When the tank is turned on, make sure it's not kinematic.
+        m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable ()
+    {
+        // When the tank is turned off, set it to kinematic so it stops moving.
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start ()
+    {
+        // The axes names are based on player number.
+        m_MovementAxisName = "Vertical";
+        m_TurnAxisName = "Horizontal";
+
+        // Store the original pitch of the audio source.
+        //m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+
+    private void Update ()
+    {
+        // Store the value of both input axes.
+        m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+
+        //EngineAudio ();
+    }
+
+
+    private void EngineAudio ()
+    {
+        // If there is no input (the tank is stationary)...
+        if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
         {
-            GetComponentInChildren<Transform>().Rotate(Vector3.right, 1f);
+            // ... and if the audio source is currently playing the driving clip...
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                // ... change the clip to idling and play it.
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play ();
+            }
         }
-        else if (Input.mousePosition.y < Screen.height / 2 - Screen.height / 8)
+        else
         {
-            GetComponentInChildren<Transform>().Rotate(Vector3.right, -1f);
+            // Otherwise if the tank is moving and if the idling clip is currently playing...
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                // ... change the clip to driving and play.
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
         }
     }
 
-    float ManageKeys(KeyCode key)
+
+    private void FixedUpdate ()
     {
-        return Input.GetKey(key) ? 1f : 0;
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        Move ();
+        Turn ();
+    }
+
+
+    private void Move ()
+    {
+        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        m_Rigidbody.MovePosition(m_Rigidbody.position - movement);
+    }
+
+
+    private void Turn ()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
     }
 }
