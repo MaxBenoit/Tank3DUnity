c96257ebc26c81d7cff96f2712197a4830d73b79
MaxBenoit <benoit.maxime@hotmail.com>
2016-05-05 15:04:00 -0400

    Bullet management beginning

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour 
+{
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Vector3 movement = transform.right * 20f * Time.deltaTime;
+        rigidbody.MovePosition(rigidbody.position - movement);
+	}
+}
---
+++ Assets/Scripts/TankAbilitys.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankAbilitys : MonoBehaviour {
+    public GameObject bullet;
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, new Vector3(rigidbody.position.x, rigidbody.position.y + 1, rigidbody.position.z), Quaternion.Euler(new Vector3(rigidbody.rotation.eulerAngles.x, rigidbody.rotation.eulerAngles.y - 90f, rigidbody.rotation.eulerAngles.z)));
+        }
+	}
+}
